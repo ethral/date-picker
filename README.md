46ed8ba1cd4c997c42a37f9937851b9f5b2435b0
ethral <abhinavsudam1@gmail.com>
2021-01-02 21:43:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-5l3mqd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-5l3mqd)
---
+++ src/app/date-range-picker-overview-example.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+/** @title Basic date range picker */
+@Component({
+  selector: 'date-range-picker-overview-example',
+  templateUrl: 'date-range-picker-overview-example.html',
+})
+export class DateRangePickerOverviewExample {}
+
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
